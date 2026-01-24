6504bff6e1e92aef84ef90a961cb944c9089d2ee
PanWor <dawideksudnicki@gmail.com>
2026-01-24 18:01:38 +0100

    chore(config): update locale and baseUrl for deployment

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,8 +15,8 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "pl-PL",
+    baseUrl: "https://panwor.github.io/notatki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
