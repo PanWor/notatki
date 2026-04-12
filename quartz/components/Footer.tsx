db899dea8d535dcaad691ad06f161daaba4120e3
PanWor <dawideksudnicki@gmail.com>
2026-04-12 20:25:40 +0200

    fix: simplify footer component and update legal notice

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,7 +15,10 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          <a href="https://quartz.jzhao.xyz/">Quartz</a> ©{year}
+        </p>
+        <p>
+          Korzystając z tej strony, przyjmujesz na siebie nieodwołalne zobowiązanie do bezzwłocznego i osobistego powiadomienia właściciela o każdym dostrzeżonym błędzie, literówce lub nieścisłości w treści notatek, uznając, że zaniechanie tego obowiązku stanowi rażące naruszenie warunków użytkowania serwisu pod rygorem pełnej odpowiedzialności cywilno-moralnej.
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
